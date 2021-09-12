1e1687b6b61849648d926c180d00891d71dc0e70
DimaAlabsi <dema.yaser8@gmail.com>
2021-09-12 21:26:04 +0300

    lab 06 editing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,107 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
 import Location from './components/Location';
+import axios from 'axios';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Row,col,container } from 'react-bootstrap';
+
  class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+     display_name:'',
+     lon:'',
+     lat:'',
+     showData:false,
+     mapShow:false,
+     alert:false
+  
+    }
+  }
+  handleLocation=(e)=>{
+   let  display_name=e.target.value;
+   this.setState({
+   display_name:display_name
+   })
+  }
+  handleSubmit=(e)=>{
+   e.preventDefault();
+   let config={
+     method:"GET",
+     baseURL:`https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}`
+   }
+  
+  
+  axios(config).then(res=>{
+   let responseData=res.data[0];
+   this.setState({
+    display_name:responseData.display_name,
+     lon:responseData.lon,
+     lat:responseData.lat,
+     showData:true,
+     mapShow:true,
+    
+   })
+  }
+  )
+  
+  }
+  alertError=()=>{
+    this.setState({
+      alert:true
+    })
+      }     
+ 
   render() {
     return (
-      <div>
-        <Form/>
-        <Location/>
-      </div>
+  <>
+  <Header/>
+         <Form handleLocation={this.handleLocation} handleSubmit={this.handleSubmit}/>
+     <Location display_name={this.state.display_name}
+                 lat={this.state.lat}
+                 lon={this.state.lon}  alertError={this.alertError}/>
+
+   <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=1-5`} 
+   style ={{width:550} }
+   variant="top"
+   width='300px' height='300px'
+    alt="map"/>
+
+    {/* <>
+    <alert>Sorry, cannot found</alert>
+    </> */}
+    
+    </>
     )
   }
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
