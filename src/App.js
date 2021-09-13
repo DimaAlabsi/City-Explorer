7a00525469a3ddc3574623a227fbff63fd1e1731
DimaAlabsi <dema.yaser8@gmail.com>
2021-09-13 14:34:35 +0300

    lab 06 has been done

--- src/App.js
+++ src/App.js
@@ -7,87 +7,80 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Alert } from 'react-bootstrap';
 // import { Row,col,container } from 'react-bootstrap';
 
- class App extends Component {
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-     display_name:'',
-     lon:'',
-     lat:'',
-     showData:false,
-     mapShow:false,
-     alert:false
-  
+    this.state = {
+      display_name: '',
+      lon: '',
+      lat: '',
+      showData: false,
+      mapShow: false,
+      error: {},
+      errHandle: false
     }
   }
-  handleLocation=(e)=>{
-   
-   let  display_name=e.target.value;
-   if (display_name===''){
+  handleLocation = (e) => {
+
+    let display_name = e.target.value;
+
     this.setState({
-      alert:true
+      display_name: display_name
     })
   }
-  else
- {  this.setState({
-   display_name:display_name
-   })
-  }}
-  handleSubmit=(e)=>{
-   e.preventDefault();
-   let config={
-     method:"GET",
-     baseURL:`https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}`
-   }
-  
-  
-  axios(config).then(res=>{
-   let responseData=res.data[0];
-   this.setState({
-    display_name:responseData.display_name,
-     lon:responseData.lon,
-     lat:responseData.lat,
-     showData:true,
-     mapShow:true,
-    
-   })
-  }
-  )
-  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let config = {
+      method: "GET",
+      baseURL: `https://api.locationiq.com/v1/autocomplete.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.display_name}`
+    }
+
+
+    axios(config).then(res => {
+      let responseData = res.data[0];
+      this.setState({
+        display_name: responseData.display_name,
+        lon: responseData.lon,
+        lat: responseData.lat,
+        showData: true,
+        mapShow: true,
+
+      })
+    }
+    ).catch(err => this.setState({
+      error: err.toString(), errHandle: true
+    }))
+
   }
   // alertError=()=>{
   //   this.setState({
   //     alert:true
   //   })
   //     }     
- 
+
   render() {
     return (
-  <>
-  <Header/>
-         <Form handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} />
-     <Location display_name={this.state.display_name}
-                 lat={this.state.lat}
-                 lon={this.state.lon}  alertError={this.alertError}/>
+      <>
+        <Header />
+        <Form handleLocation={this.handleLocation} handleSubmit={this.handleSubmit} />
+        <Location display_name={this.state.display_name}
+          lat={this.state.lat}
+          lon={this.state.lon} />
 
-   <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=1-5`} 
-   style ={{width:550} }
-   variant="top"
-   width='300px' height='300px'
-    alt="map"/>
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=1-5`}
+          style={{ width: 550 }}
+          variant="top"
+          width='300px' height='300px'
+          alt="map" />
+{
+  this.state.errHandle && <Alert>
+    {this.state.error}
+  </Alert>
+}
+       
 
-   {this.state.alert &&
-   <>
-   <Alert>
 
-     Sorry. the location does not FOUND
-   </Alert>
-   </>
-   
-   
-   }
-    
-    </>
+      </>
     )
   }
 }
